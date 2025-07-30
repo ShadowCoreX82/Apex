52d4352c904852259b266312fd6fc561285d1c2a
ShadowCoreX82 <timok1393@gmail.com>
2025-07-30 11:12:32 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
 // Dies ist ein Kommentar in JavaScript
 console.log("Die Website wurde geladen!");
 
-// Beispiel: Eine einfache Begrüßung beim Laden der Seite
-window.onload = function() {
-    alert("Willkommen auf meiner selbstprogrammierten Seite!");
-};
